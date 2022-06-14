92bc6582a593a01105f8ab280e038c59b9d965bd
Iain Earl <iain@itmecho.com>
2022-06-14 13:18:53 +0100

    Trigger custom autocommands on key neoterm events
    Update docs

--- lua/neoterm.lua
+++ lua/neoterm.lua
@@ -1,24 +1,28 @@
 local state = {
-    winh = nil,
-    bufh = nil,
-    chan = nil,
-    last_command = nil
+  winh = nil,
+  bufh = nil,
+  chan = nil,
+  last_command = nil,
 }
 
 local config = {
-    clear_on_run = true,
-    mode = "vertical",
-    noinsert = false
+  clear_on_run = true,
+  mode = "vertical",
+  noinsert = false,
 }
 
 -- Returns a bool to show if the neoterm window exists
 local function win_is_open()
-    return state.winh ~= nil and vim.api.nvim_win_is_valid(state.winh) == true
+  return state.winh ~= nil and vim.api.nvim_win_is_valid(state.winh) == true
 end
 
 -- returns a bool to show if the buf exists
 local function buf_is_valid()
-    return state.bufh ~= nil and vim.api.nvim_buf_is_valid(state.bufh) == true
+  return state.bufh ~= nil and vim.api.nvim_buf_is_valid(state.bufh) == true
+end
+
+local function fire_event(event)
+  vim.api.nvim_exec_autocmds("User", { pattern = event })
 end
 
 local neoterm = {}
@@ -32,8 +36,8 @@ local neoterm = {}
 --		* fullscreen
 --	noinsert - don't enter insert mode when switching to the neoterm window
 function neoterm.setup(opts)
-    config.mode = opts.mode or config.mode
-    config.noinsert = opts.noinsert or config.noinsert
+  config.mode = opts.mode or config.mode
+  config.noinsert = opts.noinsert or config.noinsert
 end
 
 -- Opens the terminal window. If it was opened previously, the same terminal buffer will be used
@@ -41,112 +45,140 @@ end
 --	mode - override the global config mode
 --	noinsert - don't enter insert mode after switching to the neoterm window
 function neoterm.open(opts)
-    opts = opts or {}
+  opts = opts or {}
 
-    local buf_created = false
-    if buf_is_valid() == false then
-        state.bufh = vim.api.nvim_create_buf(false, true)
-        buf_created = true
+  local buf_created = false
+  if buf_is_valid() == false then
+    state.bufh = vim.api.nvim_create_buf(false, true)
+    buf_created = true
+  end
+
+  local group = vim.api.nvim_create_augroup("Neoterm", {})
+
+  if win_is_open() ~= true then
+    local ui = vim.api.nvim_list_uis()[1]
+
+    local mode = opts.mode or config.mode
+
+    local winopts = {
+      relative = "editor",
+      width = math.floor(ui.width / 2),
+      height = ui.height - vim.o.cmdheight - 3,
+      row = 0,
+      col = ui.width,
+      style = "minimal",
+      border = "single",
+    }
+    if mode == "horizontal" then
+      winopts.width = ui.width
+      winopts.height = math.floor((ui.height - vim.o.cmdheight - 2) / 3)
+      winopts.row = (2 * winopts.height)
+      winopts.col = 1
+    elseif mode == "fullscreen" then
+      winopts.width = ui.width
+      winopts.col = 1
     end
 
-    if win_is_open() ~= true then
-        local ui = vim.api.nvim_list_uis()[1]
+    state.winh = vim.api.nvim_open_win(state.bufh, true, winopts)
+    fire_event("NeotermWinOpen")
+    vim.api.nvim_create_autocmd("WinClosed", {
+      pattern = string.format("%d", state.winh),
+      callback = function()
+        fire_event("NeotermWinClose")
+      end,
+      group = group,
+    })
 
-        local mode = opts.mode or config.mode
-
-        local winopts = {
-            relative = "editor",
-            width = math.floor(ui.width / 2),
-            height = ui.height - vim.o.cmdheight - 3,
-            row = 0,
-            col = ui.width,
-            style = "minimal",
-            border = "single"
-        }
-        if mode == "horizontal" then
-            winopts.width = ui.width
-            winopts.height = math.floor((ui.height - vim.o.cmdheight - 2) / 3)
-            winopts.row = (2 * winopts.height)
-            winopts.col = 1
-        elseif mode == "fullscreen" then
-            winopts.width = ui.width
-            winopts.col = 1
-        end
-
-        state.winh = vim.api.nvim_open_win(state.bufh, true, winopts)
-
-        if buf_created then
-            vim.cmd [[term]]
-            state.chan = vim.b.terminal_job_id
-            vim.api.nvim_buf_set_name(state.bufh, "neoterm")
-            vim.api.nvim_buf_set_option(state.bufh, "buflisted", false)
-        end
+    if buf_created then
+      vim.cmd([[term]])
+      state.chan = vim.b.terminal_job_id
+      vim.api.nvim_buf_set_name(state.bufh, "neoterm")
+      vim.api.nvim_buf_set_option(state.bufh, "buflisted", false)
     end
+  end
 
-    local noinsert = config.noinsert
-    if opts.noinsert ~= nil then
-        noinsert = opts.noinsert
-    end
+  local noinsert = config.noinsert
+  if opts.noinsert ~= nil then
+    noinsert = opts.noinsert
+  end
 
-    if noinsert == false then
-        vim.cmd [[startinsert]]
-    end
+  if noinsert == false then
+    vim.cmd([[startinsert]])
+  end
+
+  fire_event("NeotermOpened")
+
+  vim.api.nvim_create_autocmd("TermEnter", {
+    buffer = state.bufh,
+    callback = function()
+      fire_event("NeotermTermEnter")
+    end,
+    group = group,
+  })
+
+  vim.api.nvim_create_autocmd("TermLeave", {
+    buffer = state.bufh,
+    callback = function()
+      fire_event("NeotermTermLeave")
+    end,
+    group = group,
+  })
 end
 
 function neoterm.close()
-    vim.api.nvim_win_close(state.winh, false)
-    state.winh = nil
+  vim.api.nvim_win_close(state.winh, false)
+  state.winh = nil
 end
 
 function neoterm.toggle()
-    if win_is_open() then
-        neoterm.close()
-    else
-        neoterm.open()
-    end
+  if win_is_open() then
+    neoterm.close()
+  else
+    neoterm.open()
+  end
 end
 
 function neoterm.exit()
-    neoterm.close()
+  neoterm.close()
 
-    vim.api.nvim_chan_send(state.chan, "exit\n")
-    state.chan = nil
+  vim.api.nvim_chan_send(state.chan, "exit\n")
+  state.chan = nil
 
-    vim.api.nvim_buf_delete(state.bufh, {force = true})
-    state.bufh = nil
+  vim.api.nvim_buf_delete(state.bufh, { force = true })
+  state.bufh = nil
+
+  fire_event("NeotermExit")
 end
 
 -- Takes a command as a string and runs it in the neoterm buffer. If the window is closed, it will be toggled
 -- Options:
 --	clear - send clear command before running the given command
 function neoterm.run(command, opts)
-    opts =
-        opts or
-        {
-            clear = config.clear_on_run
-        }
+  opts = opts or {
+    clear = config.clear_on_run,
+  }
 
-    if win_is_open() == false or state.chan == nil then
-        neoterm.open()
-    end
+  if win_is_open() == false or state.chan == nil then
+    neoterm.open()
+  end
 
-    if state.last_command ~= nil then
-        -- Send <C-c> to make sure any on-going commands like log tails are stopped before running the new command
-        vim.api.nvim_chan_send(state.chan, "\003\n")
-    end
-    state.last_command = command
+  if state.last_command ~= nil then
+    -- Send <C-c> to make sure any on-going commands like log tails are stopped before running the new command
+    vim.api.nvim_chan_send(state.chan, "\003\n")
+  end
+  state.last_command = command
 
-    if opts.clear then
-        vim.api.nvim_chan_send(state.chan, "clear\n")
-    end
-    vim.api.nvim_chan_send(state.chan, command .. "\n")
+  if opts.clear then
+    vim.api.nvim_chan_send(state.chan, "clear\n")
+  end
+  vim.api.nvim_chan_send(state.chan, command .. "\n")
 end
 
 function neoterm.rerun()
-    if state.last_command == nil then
-        print("Last command empty")
-    end
-    neoterm.run(state.last_command)
+  if state.last_command == nil then
+    print("Last command empty")
+  end
+  neoterm.run(state.last_command)
 end
 
 return neoterm
