ae28412539f84e9b7f38f0b8ca9d53b4e2a03826
dpi0 <dpi0.dev@proton.me>
2024-07-04 10:50:05 +0530

    Fixed deprecated vim.api.nvim_buf_set_option usage

--- lua/neoterm.lua
+++ lua/neoterm.lua
@@ -99,7 +99,7 @@ function neoterm.open(opts)
       vim.cmd([[term]])
       state.chan = vim.b.terminal_job_id
       vim.api.nvim_buf_set_name(state.bufh, "neoterm")
-      vim.api.nvim_buf_set_option(state.bufh, "buflisted", false)
+      vim.api.nvim_set_option_value('buflisted', false, { buf = state.bufh })
     end
   end
 
