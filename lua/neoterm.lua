6df63dc011b0586235eb999f578ad73df8b44cf2
dpi0 <dpi0.dev@proton.me>
2024-07-04 18:53:37 +0530

    Added height option

--- lua/neoterm.lua
+++ lua/neoterm.lua
@@ -11,6 +11,7 @@ local config = {
   mode = "vertical",
   noinsert = false,
   width = 1 / 2,
+  height = 1 / 3
 }
 
 -- Returns a bool to show if the neoterm window exists
@@ -37,16 +38,21 @@ local neoterm = {}
 --		* horizonal
 --		* fullscreen
 --	noinsert - don't enter insert mode when switching to the neoterm window
+--| width - set the width of the terminal window
+--| height - set the height of the terminal window
 function neoterm.setup(opts)
   config.mode = opts.mode or config.mode
   config.noinsert = opts.noinsert or config.noinsert
   config.width = opts.width or config.width
+  config.height = opts.height or config.height
 end
 
 -- Opens the terminal window. If it was opened previously, the same terminal buffer will be used
 -- Options:
 --	mode - override the global config mode
 --	noinsert - don't enter insert mode after switching to the neoterm window
+--	width - override the global config width
+--	height - override the global config height
 function neoterm.open(opts)
   opts = opts or {}
 
@@ -63,6 +69,7 @@ function neoterm.open(opts)
 
     local mode = opts.mode or config.mode
     local width = opts.width or config.width
+    local height = opts.height or config.height
 
     local winopts = {
       relative = "editor",
@@ -75,9 +82,9 @@ function neoterm.open(opts)
     }
     if mode == "horizontal" then
       winopts.width = ui.width
-      winopts.height = math.floor((ui.height - vim.o.cmdheight - 2) / 3)
-      winopts.row = (2 * winopts.height)
-      winopts.col = 1
+      winopts.height = math.floor(ui.height * height)
+      winopts.row = ui.height - winopts.height - vim.o.cmdheight - 2
+      winopts.col = 0
     elseif mode == "fullscreen" then
       winopts.width = ui.width
       winopts.col = 1
